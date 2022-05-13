f936b7879f3bb2c40e98ce8dbcac4d8c618cfade
Starsky <starsky.m@live.com>
2022-05-13 10:16:42 +0200

    Initial Commit

---
+++ Shared/NumberConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberConverterApp.swift
+//  Shared
+//
+//  Created by star on 13.05.22.
+//
+
+import SwiftUI
+
+@main
+struct NumberConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
